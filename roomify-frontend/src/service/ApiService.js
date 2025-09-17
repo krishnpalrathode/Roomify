d79ce41db96f36a6f9f5d9cd6a3f79f1e886a26d
krishnpalrathode <rajputkrishna9399@gmail.com>
2025-09-17 09:14:19 +0530

    Initial commit

--- roomify-frontend/src/service/ApiService.js
+++ roomify-frontend/src/service/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default class ApiService {
 
-    static BASE_URL = "http://localhost:4040"
+    static BASE_URL = "http://localhost:8080"
 
     static getHeader() {
         const token = localStorage.getItem("token");
